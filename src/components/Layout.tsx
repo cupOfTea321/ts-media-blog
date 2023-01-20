6effd419fbdad82db99aa75aa111a92e3dd18c6f
Alex <ddragon99999@gmail.com>
2023-01-20 13:32:43 +0300

    added mainPage design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import {NavLink, Outlet} from "react-router-dom";
-
+import  './Layout.css'
 const Layout = () => {
     return (
-        <>
-            <header>
-                <nav>
-                    <NavLink to={'/'} >Main page</NavLink>
-                    <NavLink to={'/todo'} >Todos page</NavLink>
-                </nav>
-            </header>
-            <main className={'container'}>
-                <Outlet/>
-            </main>
-        </>
+        <div className={'mainRoot'}>
+            <div className={'appMain'}>
+                <header>
+                    <h1>FlexMess</h1>
+                    <nav className={'nav'}>
+                        <NavLink to={'/'} >Main page</NavLink>
+                        <NavLink to={'/todo'} >Todos page</NavLink>
+                    </nav>
+                    <div className={'logIn'}>
+                        <a href="#s">log In</a>
+                        <button value="log In" className="buttonUse">Getting Started</button>
+                    </div>
+                </header>
+                <main className={'container'}>
+                    <Outlet/>
+                </main>
+            </div>
+        </div>
+
 
 
     );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import './Main.css'
 const MainPage: React.FC = () => {
     return (
-        <div>
-            MainPage
-        </div>
+        <main>
+            <div className="left-side">
+                <div className="left-side-msg">
+                    <h2><span>Grow your media with</span> FlexMess!</h2>
+                    <p>This organization is non-profit.</p>
+                    <p>All data is protected!</p>
+                </div>
+                <div className="forms">
+                    <input className="inputMail" placeholder="Email Adress" type="text" />
+                    <button className="buttonUse">Learn More</button>
+
+                </div>
+            </div>
+
+            <div className="right-side">
+
+            </div>
+        </main>
     );
 };
 
